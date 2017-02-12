1e0463465720cd8d413ba56e188415cd2c13d8be
yashacb <yashwanthbetha@gmail.com>
2017-02-12 06:16:52 +0530

    can read commands from stdin and files .

--- a.c
+++ a.c
@@ -11,23 +11,30 @@ int main(int argc , char* argv[])
 	initial_gd = atoi(argv[1]) ;
 	bucket_size = atoi(argv[2]) ;
 	dir* d = create_directory(initial_gd) ;
-	int t = insert(d , 8) ;
-	printf("Inserted %d in %d\n", 8 , t);
-	status(d) ;
-	t = insert(d , 16) ;
-	printf("Inserted %d in %d\n", 16 , t);
-	status(d) ;
-	t = insert(d , 4) ;
-	printf("Inserted %d in %d\n", 4 , t);
-	status(d) ;
-	t = insert(d , 9) ;
-	printf("Inserted %d in %d\n", 9 , t);
-	status(d) ;
-	t = insert(d , 17) ;
-	printf("Inserted %d in %d\n", 17 , t);
-	status(d) ;
-	t = insert(d , 5) ;
-	printf("Inserted %d in %d\n", 5 , t);
-	status(d) ;
+	int val ;
+	char first = fgetc(stdin) ;
+	while(1)
+	{
+		switch(first)
+		{
+			case 'i' :
+				scanf("nsert %d" , &val) ;
+				insert(d , val) ;
+				break ;
+			case 's' :
+				first = getchar() ;
+				if(first == 'e')
+					/*do search */ ;
+				else
+					status(d) ;
+				break ;
+			case 'd' :
+				/*do delete*/ ;
+				break ;
+			case EOF :
+				exit(0) ;
+		}
+		first = fgetc(stdin) ;
+	}
 	return 0 ;
 }
